e45517732a0ae4a3ca49e1ba1b66699f61d1246b
Ooi Liang Sun <liangsun@gmail.com>
2018-02-17 22:01:22 +0800

    Fix msg for processing

--- ciscosparkwebhook.py
+++ ciscosparkwebhook.py
@@ -1,9 +1,6 @@
-import hmac
-import hashlib
-
 from errbot import BotPlugin, webhook
-import ciscosparkapi as sparkapi
-
+from ciscosparkapi import CiscoSparkAPI, Webhook
+spark_api = CiscoSparkAPI()
 
 class CiscoSparkWebhook(BotPlugin):
 
@@ -16,27 +13,31 @@ class CiscoSparkWebhook(BotPlugin):
             self.log.debug("X-Spark-Signature failed. Webhook will NOT be processed")
 
         else:
-            webhook_event = sparkapi.Webhook(raw.json)
+            webhook_event = Webhook(raw.json)
+
+            bot_name = self._bot.bot_name
 
             if webhook_event.actorId == self._bot.bot_identifier.id:
                 self.log.debug("Message created by bot...ignoring")
 
             else:
 
-                # Need to load to complete message from Spark as the webhook message only includes IDs
-                message = self._bot.get_message_using_id(webhook_event.data.id)
+                # Need to load complete message from Spark as the webhook message only includes IDs
+                # Can only retrieve messages targeted to the bot. User must type @<botname> <message>.
+                message = spark_api.messages.get(webhook_event.data.id)   # Get the message details
 
-                # We only build superficial objects using the ID as loading the full person/room details would
-                # significantly delay the processing of the message
-                person = self._bot.create_person_using_id(webhook_event.data.personId)
-                room = self._bot.create_room_using_id(webhook_event.data.roomId)
-                
+                #spark_api.messages.create(webhook_event.data.roomId, text="Welcome to the room!")
+
+                room = spark_api.rooms.get(webhook_event.data.roomId)     # Get the room details
+                person = spark_api.people.get(message.personId)           # Get the sender's details
                 occupant = self._bot.get_occupant_using_id(person=person, room=room)
 
-                msg = self._bot.create_message(body=message.text, frm=occupant, to=room,
-                                               extras={'roomType': webhook_event.data.roomType})
+                # Strip the bot name from the message in order for errbot to process commands properly
+                message_without_botname = message.text.replace(bot_name,"",1).lstrip()
+                msg = self._bot.create_message(body=message_without_botname, frm=occupant, to=room, extras={'roomType': webhook_event.data.roomType})
 
                 # Force the bot to process the message and our job is done!
                 self._bot.process_message(msg)
 
         return "OK"
+
